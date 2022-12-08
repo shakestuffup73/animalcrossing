43a75571efc76339bb1855ab687649eb90efb1f6
Amanda Stern <amanda.stern.73@gmail.com>
2022-12-08 13:18:14 -0500

    access museum phrase hyphenated key name with brackets

--- src/components/Bug/Bug.jsx
+++ src/components/Bug/Bug.jsx
@@ -1,6 +1,10 @@
 const Bug = ({bug}) => {
-  return ( 
-    <h2>{bug.name["name-USen"]}</h2>
+  return (
+    <div id="bugdiv">
+      <h2>{bug.name["name-USen"]}</h2>
+      <img src={bug.image_uri} alt="bug" />
+      <h4>{bug['museum-phrase']}</h4>
+    </div> 
   );
 }
 
